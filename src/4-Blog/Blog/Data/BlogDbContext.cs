5366d323a0f5752ed5d300a6d5120f6a99af1450
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2023-10-20 01:23:46 -0300

    feat(database): Add BlogDbContext and EntityConfigs

---
+++ src/4-Blog/Blog/Data/BlogDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Data.EntityConfig;
+using Blog.Models;
+using Blog.Models.Recipes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data;
+
+public class BlogDbContext : DbContext
+{
+    public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options) { }
+
+    public DbSet<RecipeBlog> Recipes { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new RecipeBlogConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
---
+++ src/4-Blog/Blog/Data/EntityConfig/CategoryConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.EntityConfig;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("categories");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(c => c.Id)
+            .HasColumnName("id");
+
+        builder.Property(c => c.Name)
+            .HasColumnName("name")
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(rb => rb.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired();
+
+        builder.Property(rb => rb.UpdatedAt)
+            .HasColumnName("updated_at")
+            .IsRequired();
+
+        builder.HasMany(c => c.Blogs)
+               .WithOne(rb => rb.Category)
+               .HasForeignKey(rb => rb.CategoryId);
+    }
+}
