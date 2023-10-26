cc244a03df198b7feb9225d0ab858f6d8c0accc9
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-10-26 20:14:08 -0300

    fix: Correct corrupted file

---
+++ src/3-Data/Data/Repositories/Blog/RecipePostRepository.cs
@@ -0,0 +1,13 @@
+﻿using Business.Interfaces.Repositories.Blog;
+using Business.Models.Blog.Recipe;
+using Data.Configuration;
+
+namespace Data.Blog.Repositories;
+
+public class RecipePostRepository : BaseRepository<RecipePost>, IRecipePostRepository
+{
+
+    public RecipePostRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
