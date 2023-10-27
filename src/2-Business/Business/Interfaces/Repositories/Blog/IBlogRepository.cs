693cbaede7c66882e79c407f0b389174a1795811
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-10-26 21:10:20 -0300

    feat(database): Add BlogRepository

---
+++ src/2-Business/Business/Interfaces/Repositories/Blog/IBlogRepository.cs
@@ -0,0 +1,8 @@
+﻿using Business.Models.Blog;
+
+namespace Business.Interfaces.Repositories.Blog;
+
+public interface IBlogRepository : IBaseRepository<BlogEntity>
+{
+    Task<BlogEntity> GetBlogByName(string blogName);
+}
---
+++ src/3-Data/Data/Repositories/Blog/BlogRepository.cs
@@ -0,0 +1,23 @@
+﻿using Business.Interfaces.Repositories.Blog;
+using Business.Models.Blog;
+using Data.Configuration;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Blog.Repositories;
+
+public class BlogRepository : BaseRepository<BlogEntity>, IBlogRepository
+{
+
+    public BlogRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<BlogEntity> GetBlogByName(string blogName)
+    {
+        var blogNameNormalized = blogName.ToLower();
+        var blogDb = await _context.Blogs.FirstOrDefaultAsync(b => b.NormalizedName == blogNameNormalized);
+        if (blogDb == null) return null!;
+        return blogDb;
+    }
+}
