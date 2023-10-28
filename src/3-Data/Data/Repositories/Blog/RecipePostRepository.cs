e8037838f1482e3a44ecbd7c96a917c466017a95
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-10-28 10:03:25 -0300

    feat(database): Add GetRecipeByTitle method

--- src/3-Data/Data/Repositories/Blog/RecipePostRepository.cs
+++ src/3-Data/Data/Repositories/Blog/RecipePostRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Business.Interfaces.Repositories.Blog;
+using Business.Models.Blog;
 using Business.Models.Blog.Recipe;
 using Data.Configuration;
 using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Blog.Repositories;
 
@@ -10,5 +12,12 @@ public class RecipePostRepository : BaseRepository<RecipePost>, IRecipePostRepos
 
     public RecipePostRepository(ApplicationDbContext context) : base(context)
     {
+    }
+
+    public async Task<RecipePost> GetRecipeByTitle(string title)
+    {
+        var recipeDb = await _context.Recipes.FirstOrDefaultAsync(b => b.Title == title);
+        if (recipeDb == null) return null!;
+        return recipeDb;
     }
 }
