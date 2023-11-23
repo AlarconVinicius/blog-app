cef6d2938d442b18f461de64341f066e154950dc
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-11-23 19:56:03 -0300

    feat(SPA): Add LocalStorageUtils

---
+++ src/Web/WebSPA/Blog/src/app/helper/localstorage/localstorage.ts
@@ -0,0 +1,46 @@
+import { ClaimsResponse, LoginResponse } from "src/app/models/auth/login";
+
+export class LocalStorageUtils {
+
+    public getUser() {
+        return JSON.parse(localStorage.getItem('user') || '{}');
+    }
+
+    public saveLocalUserData(response: LoginResponse) {
+        this.saveUserToken(response.accessToken);
+        this.saveUserClaims(response.userToken.claims);
+    }
+
+    public clearLocalUserData() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('claims');
+    }
+
+    public getUserToken(): string {
+        return localStorage.getItem('token') || '';
+    }
+    public getUserClaims(): ClaimsResponse[] {
+        const claimsString = localStorage.getItem('claims') || '[]';
+        return JSON.parse(claimsString) as ClaimsResponse[];
+    }
+
+    public saveUserToken(token: string) {
+        localStorage.setItem('token', token);
+    }
+
+    public saveUserClaims(claims: ClaimsResponse[]) {
+        localStorage.setItem('claims', JSON.stringify(claims));
+    }
+
+    public hasWriterPermission(): boolean {
+        const claims = this.getUserClaims();
+        return claims.some(claim => claim.type === 'Permission' && claim.value.includes('Writer'));
+    }
+
+    public isLoggedIn(): boolean{
+        if(Boolean(this.getUserToken())){
+            return true;
+        }
+        return false
+    }
+}
