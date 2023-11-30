62a5df369a44b750e8e2747e85b9ae4021da2bd1
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-11-30 13:48:56 -0300

    feat(SPA): Redesign homepage

--- src/Web/WebSPA/Blog/src/app/features/site/home/home.component.ts
+++ src/Web/WebSPA/Blog/src/app/features/site/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { RecipeResponse } from 'src/app/core/models/recipe/recipe.model';
-import { RecipeService } from 'src/app/shared/services/recipe/recipe.service';
 
 @Component({
   selector: 'app-home',
@@ -10,39 +7,13 @@ import { RecipeService } from 'src/app/shared/services/recipe/recipe.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title: string = '';
-  showNoRecipesMessage: boolean = false;
-  recipes$ = new Observable<RecipeResponse[]>();
-  recipe = {} as RecipeResponse;
   search: string = '';
-  constructor(private recipeService: RecipeService, private router: Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.title = 'Últimas Receitas';
-    this.getRecipes();
-  }
-
-  getRecipes(){
-    this.recipeService.getPublicRecipes().subscribe(recipes => {
-      this.recipes$ = of(recipes);
-      this.hasRecipes(recipes);
-    });
-  }
-  getRecipe(recipe: RecipeResponse){
-    this.router.navigate([`receita/${recipe.id}`]);
   }
   onSearch(){
-    this.title =  `Resultado da busca por: ${this.search}`;
-    this.getRecipeBySearch(this.search);
-    this.search = '';
-  }
-  getRecipeBySearch(search: string){
-    this.recipeService.getPublicRecipesBySearch(search).subscribe(recipes => {
-      this.recipes$ = of(recipes);
-      this.hasRecipes(recipes);
-    });
-  }
-  hasRecipes(recipes: RecipeResponse[]){
-    this.showNoRecipesMessage = !recipes || recipes.length === 0;
+    this.router.navigate([`receitas/busca/${this.search}`]);
+    this.search = ''
   }
 }
