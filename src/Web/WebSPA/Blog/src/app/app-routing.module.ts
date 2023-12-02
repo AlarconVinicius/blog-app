7d3bc7a1be17cb7f319a4bc0aef00d78bb41bf81
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2023-12-02 11:22:09 -0300

    feat(SPA): Remove unused routes

--- src/Web/WebSPA/Blog/src/app/app-routing.module.ts
+++ src/Web/WebSPA/Blog/src/app/app-routing.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlertsComponent } from './core/components/alerts/alerts.component';
-import { ContactComponent } from './features/admin/contacts/contact/contact.component';
 
 import { HomeComponent } from './features/site/home/home.component';
 
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 
-import { DashboardComponent } from './features/admin/dashboard/dashboard.component';
 import { RecipeComponent } from './features/admin/recipes/recipe/recipe.component';
 import { AddUpdRecipeComponent } from './features/admin/recipes/add-upd/add-upd-recipe.component';
 import { CategoryComponent } from './features/admin/categories/category/category.component';
 import { UsersProfileComponent } from './features/admin/users-profile/users-profile.component';
-import { FaqComponent } from './features/admin/faq/faq.component';
 import { Error404Component } from './features/error/error404/error404.component';
 import { RecipeDetailsComponent } from './features/site/recipe-details/recipe-details.component';
 import { AuthGuard } from './core/guards/auth.guard';
@@ -25,15 +22,12 @@ const authRoutes: Routes = [
   { path: 'registrar', component: RegisterComponent }
 ];
 const adminRoutes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: RecipeComponent },
   { path: 'receitas', component: RecipeComponent },
   { path: 'receitas/adicionar', component: AddUpdRecipeComponent },
   { path: 'receitas/editar/:id', component: AddUpdRecipeComponent },
   { path: 'categorias', component: CategoryComponent },
-  { path: 'perfil', component: UsersProfileComponent },
-  { path: 'contato', component: ContactComponent },
-  { path: 'faq', component: FaqComponent }
+  { path: 'perfil', component: UsersProfileComponent }
 ];
 
 const routes: Routes = [
