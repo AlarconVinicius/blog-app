1e0e46652810c1d60ea67888694a8379058bb6a8
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-12-20 19:53:49 -0300

    feat(business): Add Image Dtos to RecipePost

--- src/Services/Blog/2-Business/Business/Models/Blog/Dtos/RecipePostAddDto.cs
+++ src/Services/Blog/2-Business/Business/Models/Blog/Dtos/RecipePostAddDto.cs
@@ -1,12 +1,8 @@
 ﻿using Business.Models.Blog.Recipe;
-using Newtonsoft.Json;
+using Business.Models.Image;
 
 namespace Business.Models.Blog.Dtos;
 
-public record RecipePostAddDto(string Title, string PreparationSteps, Guid BlogId, Guid CategoryId, Difficulty Difficulty, string PreparationTime, int Servings, string Ingredients)
+public record RecipePostAddDto(string Title, string PreparationSteps, Guid BlogId, Guid CategoryId, Difficulty Difficulty, string PreparationTime, int Servings, string Ingredients, ImageAddDto Image)
 {
-    // public static string PrepStepsToString(List<string> prepSteps)
-    // {
-    //     return JsonConvert.SerializeObject(prepSteps);
-    // }
 };
